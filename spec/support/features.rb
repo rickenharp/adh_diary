ae0116d0767f081ab2696302fe2026195226e385
Alexander Gräfe <alexander.graefe@googlemail.com>
2025-07-14 17:16:10 +0200

    ci: increase cuprite timeout

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -3,8 +3,9 @@
 require "capybara/rspec"
 require "capybara/cuprite"
 
+Capybara.default_max_wait_time = 20
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], timeout: 20)
+  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], timeout: 20, process_timeout: Capybara.default_max_wait_time)
 end
 Capybara.javascript_driver = :cuprite
 
