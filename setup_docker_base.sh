40cf2c225028b7f0a29f3550bd1ee4b0494882d6
Alexander Gräfe <alexander.graefe@googlemail.com>
2025-07-21 11:32:00 +0200

    chore(docker): reduce image size
    switch to ruby-slim and optimize docker layers
    
    Closes: 59

---
+++ setup_docker_base.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p log public/assets
+# Install base packages
+ruby -r'open-uri' -e 'URI.open("https://deb.nodesource.com/setup_lts.x"){ File.write("nodesource_setup.sh", it.read)}' && bash nodesource_setup.sh
+apt-get update -qq && \
+    apt-get upgrade -y && \
+    apt-get install --no-install-recommends -y curl libjemalloc2 sqlite3 nodejs && \
+    rm -rf /var/lib/apt/lists /var/cache/apt/archives
