453ec6c75fb0abe2a9120ef0d6d47de19a1d8a66
Alexander Gräfe <alex@alexandergraefe.de>
2025-07-30 11:57:52 +0200

    build: add pg libs to Dockerfile

--- setup_docker_base.sh
+++ setup_docker_base.sh
@@ -5,5 +5,5 @@ mkdir -p log public/assets
 ruby -r'open-uri' -e 'URI.open("https://deb.nodesource.com/setup_lts.x"){ File.write("nodesource_setup.sh", it.read)}' && bash nodesource_setup.sh
 apt-get update -qq && \
     apt-get upgrade -y && \
-    apt-get install --no-install-recommends -y curl libjemalloc2 sqlite3 nodejs && \
+    apt-get install --no-install-recommends -y curl libjemalloc2 sqlite3 nodejs pgloader postgresql-client libpq-dev && \
     rm -rf /var/lib/apt/lists /var/cache/apt/archives
